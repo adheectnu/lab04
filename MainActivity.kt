34942869162f6891fa709ebdaad11ea52cd62acd
Imanol Mollehuanca <imanol.mollehuanca@tecsup.edu.pe>
2025-09-15 17:39:59 -0500

    EH CAMBIADO EL CODIGO boludo

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,20 +1,60 @@
-fun ViewHolaCurso() {
+package com.example.lab04
+
+import android.R.attr.text
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.moviecounter.ui.theme.MovieCounterTheme
+import androidx.compose.material3.Button
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.Card
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.saveable.rememberSaveable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MovieCounter()
+        }
+    }
+}
+@Composable
+fun MovieCounter(modifier: Modifier = Modifier) {
+    var count by remember { mutableStateOf(0) }
     Column(
-        modifier = Modifier
-            .fillMaxWith()   // ❌ Error: debería ser fillMaxWidth()
-            .padding(16.dp),
+        modifier = modifier.padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-       Text(
-    text = "Welcome to the Course!",
-    fontSize = 32.sp,
-    fontWeight = FontWeight.Bold,
-    color = Color.Blue
-)
-        Spacer(modifier = Modifier.heigh(16.dp))   // ❌ Error: debería ser height()
-        Text(
-            text = "Hello, Student!",
-            fontSize = 20.xD   // ❌ Error: debería ser 20.sp
-        )
+        Text(text = "You have added $count movies.")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { count++ }) {
+            Text("Add Movie")
+        }
     }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMovieCounter() {
+    MovieCounter()
 }
